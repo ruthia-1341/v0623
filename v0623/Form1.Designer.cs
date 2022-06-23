63caba599eed91eed5bfbeba58182571c7eeb7d1
Toya <66713857+NorauDn@users.noreply.github.com>
2022-06-23 15:36:42 +0900

    実数

--- v0623/Form1.Designer.cs
+++ v0623/Form1.Designer.cs
@@ -65,9 +65,10 @@ namespace v0623
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Yu Gothic UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label3.Location = new System.Drawing.Point(2, -1);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(38, 15);
+            this.label3.Size = new System.Drawing.Size(90, 37);
             this.label3.TabIndex = 2;
             this.label3.Text = "label3";
             // 
