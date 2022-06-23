f3879b4fe89d92ff73f19db5d62135c482fd5522
Toya <66713857+NorauDn@users.noreply.github.com>
2022-06-23 14:47:51 +0900

    Update Form1.cs

--- v0623/Form1.cs
+++ v0623/Form1.cs
@@ -42,6 +42,7 @@ namespace v0623
             label1.Left += vx*(int)1.10f;
             label1.Top += vy * (int)1.10f;
 
+
             if (label1.Left < 0 || label1.Left > ClientSize.Width)
             {
                 vx = -vx;
@@ -54,9 +55,18 @@ namespace v0623
                     label1.Text = ("┗( ・ω・´ )┓三");
                 }
 
+
                 if (label1.Top < 0 || label1.Top > ClientSize.Height)
                 {
                     vy = -vy;
+                    if (label1.Text == ("┗( ・ω・´ )┓三"))
+                    {
+                        label1.Text = ("三┏( ‵ ・ω・ )┛");
+                    }
+                    else if (label1.Text == ("三┏( ‵ ・ω・ )┛"))
+                    {
+                        label1.Text = ("┗( ・ω・´ )┓三");
+                    }
                 }
 
                 /*if (Score <= 0)
